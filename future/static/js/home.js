9f553e9a4b7bb0edb5f35d60e1b281074bee72cf
fahim01 <fahim.sust.edu@gmail.com>
2021-03-11 05:02:39 +0600

    some changes in home.js file

--- future/static/js/home.js
+++ future/static/js/home.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelector(".nav-links li");
+const navLinks = document.querySelector(".nav-Links");
+const links = document.querySelectorAll(".nav-Links li");
+
+
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle("open"); 
-});
+  links.forEach(link => {
+    link.classList.toggle("omar");
+  
+  });
+
+  });
