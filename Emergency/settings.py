fad1daed0418582843f8011941729d4af243c2a5
unknown <ffahimkhan960@gmail.com>
2021-03-10 23:15:07 +0600

    Project Updated

--- Emergency/settings.py
+++ Emergency/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(_file_)))
+STATIC_DIR = os.path.join(BASE_DIR, 'future/static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
